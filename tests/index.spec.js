6dafec136ffbce4db9e80ef28ad6acfe1e6cff16
lazarev <diedsmiling@gmail.com>
2016-10-06 13:26:09 +0300

    Tests for arguments type

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,26 +1,35 @@
 import test from 'ava' // eslint-disable-line
 import keysDiff from '../index'
 
-test('Should throw error if null is passed as argument', t => {
-  const errorMsg = 'Both arguments should be objects!'
-  t.throws(
-    () => {
-      keysDiff(null, null)
-    },
-    errorMsg
-  )
+const errorMsg = 'Both arguments should be objects!'
 
-  t.throws(
-    () => {
-      keysDiff(null, {})
-    },
-    errorMsg
-  )
+function getErrorAssertion(t, err, fn) {
+  return function assertError(foo, bar) {
+    t.throws(() => { fn(foo, bar) }, err)
+  }
+}
 
-  t.throws(
-    () => {
-      keysDiff({}, null)
-    },
-    errorMsg
-  )
+let assertError
+
+test.beforeEach(t => {
+  assertError = getErrorAssertion(t, errorMsg, keysDiff)
 })
+
+test('Should throw error if null is passed as argument', () => {
+  assertError(null, null)
+  assertError(null, {})
+  assertError({}, null)
+})
+
+test('Should throw error if integer is passed as argument', () => {
+  assertError(1, 1)
+  assertError(1, {})
+  assertError({}, 1)
+})
+
+test('Should throw error if string is passed as argument', () => {
+  assertError('foo', 'bar')
+  assertError('foo', {})
+  assertError({}, 'bar')
+})
+
